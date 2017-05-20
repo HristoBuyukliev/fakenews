c8d9d6cd74e5f9361f454487a5f2d6b212d2d080
HristoBuyukliev <hristo.buyukliev@gmail.com>
2017-05-20 11:47:36 +0300

    Add logistic regression classifier

--- BoW.py
+++ BoW.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
 
 data = pd.read_csv('data/FN_Training_Set.csv', encoding='cp1251')
 target_cols = ['fake_news_score', 'click_bait_score']
@@ -9,7 +12,11 @@ vectorizer = TfidfVectorizer(
         stop_words = stopwords, 
         encoding='cp1251', 
         ngram_range=(1,5), 
-        min_df=10)
+        min_df=100)
 
 X = vectorizer.fit_transform(data.Content.values.astype('U'))
+y = data.click_bait_score.values
+y = preprocessing.label_binarize(data.click_bait_score.values, classes = [0,1,2,3])
+lr = LogisticRegression()
+
 
