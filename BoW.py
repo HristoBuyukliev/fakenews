0f01af5a19061d93f10c2a3278b0e271ba281433
HristoBuyukliev <hristo.buyukliev@gmail.com>
2017-05-20 17:55:36 +0300

    Add ensemble

--- BoW.py
+++ BoW.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
+from sklearn.ensemble import VotingClassifier
 TRAIN_SPLIT = 2300
 N_GRAM_RANGE = 5
 
@@ -22,11 +23,17 @@ vectorizer = TfidfVectorizer(
 X = vectorizer.fit_transform(data.Content.values.astype('U'))
 y = data.fake_news_score.values
 
+# shuffle
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+X = X[indices]
+y = y[indices]
+
 x_train, x_test, y_train, y_test = train_test_split(X, y, train_size = 0.8)
 
 
 # logistic regression
-for C in [10**(i/2) for i in range(-6,7)]:
+for C in [10**(i/2.) for i in range(-6,7)]:
 	print C
 	lr = LogisticRegression(multi_class='multinomial', solver='lbfgs', C=C)
 	lr.fit(x_train, y_train)
@@ -36,24 +43,51 @@ for C in [10**(i/2) for i in range(-6,7)]:
 
 
 # SVMs
-for kernel in ['linear', 'poly', 'rbf']:
-	for C in [1000, 300, 100, 30, 10, 3, 1, 0.3, 0.1]:
+for kernel in ['linear', 'rbf']:
+	for C in [1000, 100, 30, 10, 3, 1]:
 		print C
 		print kernel
 		svm = SVC(kernel=kernel, C=C)
-		svm.fit(X[:TRAIN_SPLIT], y[:TRAIN_SPLIT])
-		predictions = svm.predict(X[TRAIN_SPLIT:])
-		print metrics.accuracy_score(predictions, y[TRAIN_SPLIT:])
+		svm.fit(x_train, y_train)
+		predictions = svm.predict(x_test)
+		print metrics.accuracy_score(predictions, y_test)
 
 # Linear regression
 lr = LinearRegression()
-lr.fit(X[:TRAIN_SPLIT], y[:TRAIN_SPLIT])
-predictions = lr.predict(X[TRAIN_SPLIT:]).round().clip(0,3)
-print metrics.accuracy_score(predictions, y[TRAIN_SPLIT:])
+lr.fit(x_train, y_train)
+predictions = lr.predict(x_test).round().clip(0,3)
+print metrics.accuracy_score(predictions, y_test)
 
 # decision tree
 for depth in [2, 5, 10]:
+	print depth
 	dt = DecisionTreeClassifier(max_depth=depth)
 	dt.fit(x_train, y_train)
-	predictions = dt.transform(x_test)
+	predictions = dt.predict(x_test)
 	print metrics.accuracy_score(y_test, predictions)
+
+# random forests
+for depth in [2, 5, 10]:
+	for n_trees in [10, 20, 50, 100, 200, 500, 1000]:
+		print depth, n_trees
+		rf = RandomForestClassifier(n_estimators=n_trees, max_depth=depth)
+		rf.fit(x_train, y_train)
+		predictions = rf.predict(x_test)
+		print metrics.accuracy_score(y_test, predictions)
+
+for depth in [10, 20]:
+	for n_trees in [1000, 5000]:
+		print depth, n_trees
+		xgb = xgboost.XGBClassifier(max_depth=depth, n_estimators=n_trees)
+		xgb.fit(x_train, y_train)
+		predictions = xgb.predict(x_test)
+		print metrics.accuracy_score(y_test, predictions)
+
+# Ensemble
+lr  = LogisticRegression(C=100)
+xgb = xgboost.XGBClassifier(max_depth=20, n_estimators=1000)
+svm = SVC(kernel='linear', C=10)
+ensemble = VotingClassifier(estimators = [('lr', lr), ('xgb', xgb), ('svm', svm)])
+for clf, label in zip([lr, xgb, svm, ensemble], ['Logistic Regression', 'xgboost', 'SVM', 'Ensemble']):
+	scores = cross_val_score(clf, X, y, cv=5, scoring='accuracy')
+	print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
