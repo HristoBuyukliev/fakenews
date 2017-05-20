5298ab2810ef62ac6655931b4e44d6dd7459b8fd
HristoBuyukliev <hristo.buyukliev@gmail.com>
2017-05-20 13:31:53 +0300

    Add support vector machines

--- BoW.py
+++ BoW.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
-
+from sklearn import metrics
 TRAIN_SPLIT = 2000
 N_GRAM_RANGE = 5
 
@@ -20,10 +21,22 @@ vectorizer = TfidfVectorizer(
 
 X = vectorizer.fit_transform(data.Content.values.astype('U'))
 y = data.click_bait_score.values
-# y = y.reshape(-1,1)
-# y = preprocessing.label_binarize(data.click_bait_score.values, classes = [0,1,2,3])
-lr = LogisticRegression(multi_class='multinomial', solver='lbfgs')
 
-lr.fit(X, y)
-cross_val_score(lr, X, y, 'accuracy')
+# logistic regression
+for C in [10**(i/2) for i in range(-6,7)]:
+	print C
+	lr = LogisticRegression(multi_class='multinomial', solver='lbfgs', C=C)
+	lr.fit(X[:TRAIN_SPLIT], y[:TRAIN_SPLIT])
+	predictions = lr.predict(X[TRAIN_SPLIT:])
+	print metrics.accuracy_score(predictions, y[TRAIN_SPLIT:])
 
+
+# SVMs
+for kernel in ['linear', 'poly', 'rbf']:
+	for C in [1000, 300, 100, 30, 10, 3, 1, 0.3, 0.1]:
+		print C
+		print kernel
+		svm = SVC(kernel=kernel, C=C)
+		svm.fit(X[:TRAIN_SPLIT], y[:TRAIN_SPLIT])
+		predictions = svm.predict(X[TRAIN_SPLIT:])
+		print metrics.accuracy_score(predictions, y[TRAIN_SPLIT:])
